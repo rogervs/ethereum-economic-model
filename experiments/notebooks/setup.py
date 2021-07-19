b40df7f2d8b9b9129841008ff42436088829f582
rogervs <rogervs@protonmail.com>
2021-07-19 18:33:34 +0100

    remove autotime

--- experiments/notebooks/setup.py
+++ experiments/notebooks/setup.py
@@ -1,17 +1,17 @@
 import sys
 import pandas as pd
-from IPython import get_ipython
+# from IPython import get_ipython
 
 
-ipython = get_ipython()
+# ipython = get_ipython()
 
 # Find performance bottlenecks by timing Python cell execution
-ipython.magic("load_ext autotime") # ipython.magic("...") is equivalent to % in Jupyter cell
+# ipython.magic("load_ext autotime") # ipython.magic("...") is equivalent to % in Jupyter cell
 
 # Reload all modules (except those excluded by %aimport) every time before executing the Python code typed
 # See https://ipython.org/ipython-doc/stable/config/extensions/autoreload.html
-ipython.magic("load_ext autoreload")
-ipython.magic("autoreload 2")
+# ipython.magic("load_ext autoreload")
+# ipython.magic("autoreload 2")
 
 # Append the root directory to Python path,
 # this allows you to store notebooks in `experiments/notebooks/` sub-directory and access model Python modules
